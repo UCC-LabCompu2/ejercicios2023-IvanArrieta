a5d3bca49808464a6192602362016616ae8e8cac
IvanArrieta <2217940@ucc.edu.ar>
2023-07-27 02:17:25 -0300

    ejercicios terminados 27/7

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -18,80 +18,76 @@ function cambiarUnidades(id,valor){
         document.cambiarUnidades.unid_yarda.value = "";
     }else if(id=="metro"){
         metro= value;
-        pulgada= 39.3701*valor;
-        pie= 3.28084*valor;
-        yarda= 1.09361*valor;
+        document.lasUnidades.unid_pulgada.value= 39.3701*valor;
+        document.lasUnidades.unid_pie.value= 3.28084*valor;
+        document.lasUnidades.unid_yarda.value= 1.09361*valor;
     }else if(id=="pulgada"){
         pulgada= value
-        metro = 0.0254*valor;
-        pie = 0.0833333*valor;
-        yarda= 0.0277778*valor;
+        document.lasUnidades.unid_pulgada.value = 0.0254*valor;
+        document.lasUnidades.unid_pie.value = 0.0833333*valor;
+        document.lasUnidades.unid_yarda.value= 0.0277778*valor;
     }else if(id=="pie"){
         pie=valor;
-        pulgada= 12*valor;
-        yarda= 0.6*valor;
-        metro = 0.3048*valor;
+        document.lasUnidades.unid_pulgada.value = 12*valor;
+        document.lasUnidades.unid_pie.value = 0.6*valor;
+        document.lasUnidades.unid_pulgada.value = 0.3048*valor;
 
     }else if(id=="yarda"){
         yarda=valor;
-        pulgada= 36*valor;
-        pie= 3*valor;
-        metro = 0.9144*valor;
+        document.lasUnidades.unid_pulgada.value = 36*valor;
+        document.lasUnidades.unid_pie.value = 3*valor;
+        document.lasUnidades.unid_yarda.value = 0.9144*valor;
     }
 }
-document.cambiarUnidades.unid_pulgada.value = Math.round(metro*100)/100;
-document.cambiarUnidades.unid_pulgada.value = Math.round(pulgada);
-document.cambiarUnidades.unid_pulgada.value = Math.round(pie);
-document.cambiarUnidades.unid_pulgada.value = Math.round(yarda);
 
 function convertirGR (id){
-    var grad;
-    var rad;
+    var grad, rad;
     if(id=="grados"){
-        rad = (grad*Math.PI)/180
         grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180
     }else if (id=="radianes"){
-        grad = (grad*Math.PI)/180
         rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI
     }
     document.getElementById("grados").value = grad;
     document.getElementById("radianes").value = rad;
 }
-function mostar_ocultar(valorMO){
-    document.getElementById("divMO").style.display = 'block';
-    if(valorMO==val_ocultar){
+function mostrar_ocultar(valorMO){
+    if(valorMO=="val_mostrar"){
+        document.getElementById("divMO").style.display = 'block';
+    }else if(valorMO=="val_ocultar"){
         document.getElementById("divMO").style.display = 'none';
     }
 }
 function calcularsuma(){
     var num1,num2;
-    num1=number (document.getElementById ("sum_num1")[0].value);
-    num2=document.getElementById ("sum_num2")[0].value;
-    document.getElementById ("sub_total")[0].innerHTML= num1+ number(num2);
+    num1=Number(document.getElementsByName ("sum_num1")[0].value);
+    num2=document.getElementsByName ("sum_num2")[0].value;
+    document.getElementsByName("sub_total")[0].value= num1 + Number(num2);
 }
 function calcularresta(){
     var num1,num2;
-    num1=number (document.getElementById ("res_num1")[0].value);
-    num2=document.getElementById ("res_num2")[0].value;
-    document.getElementById ("res_total")[0].innerHTML= num1- number(num2);
+    num1=Number(document.getElementsByName ("res_num1")[0].value);
+    num2=document.getElementsByName ("res_num2")[0].value;
+    document.getElementsByName ("res_total")[0].value= num1 - Number(num2);
 }
 function calcularmul(){
     var num1,num2;
-    num1=number (document.getElementById ("mul_num1")[0].value);
-    num2=document.getElementById ("mul_num2")[0].value;
-    document.getElementById ("mul_total")[0].innerHTML= num1* number(num2);
+    num1=Number(document.getElementsByName ("mul_num1")[0].value);
+    num2=document.getElementsByName ("mul_num2")[0].value;
+    document.getElementsByName ("mul_total")[0].value= num1 * Number(num2);
 }
 function calculardiv(){
     var num1,num2;
-    num1=number (document.getElementById ("div_num1")[0].value);
-    num2=document.getElementById ("div_num2")[0].value;
-    document.getElementById ("div_total")[0].innerHTML= num1/ number(num2);
+    num1=Number(document.getElementsByName ("div_num1")[0].value);
+    num2=document.getElementsByName ("div_num2")[0].value;
+    document.getElementsByName ("div_total")[0].value= num1 / Number(num2);
 }
 function cargarWeb(){
     var cant, unidad, urlcomp;
     cant= document.getElementById("distancia").value;
-    unidad= document.getElementById("unidades")[0].value;
-    urlcomp= "segundaWeb. #" +cant + "#" + unidad;
+    unidad= document.getElementsByName("unidades")[0].value;
+    urlcomp= "segundaWeb.html#" +cant + "#" + unidad;
     window.open(urlcomp);
 }
 function cargarResultado(){
@@ -99,8 +95,7 @@ function cargarResultado(){
     urlcomp= window.location.href.split("/")[5];
     can= urlcomp.split("#")[1];
     un= urlcomp.split("#")[2];
-    document.getElementById("dist").value= can +" " + un;
-
+    document.getElementById("dist").value= can + " " + un;
     consul.log(urlcomp);
 }
 function guardarLocalStorage(){
@@ -116,4 +111,115 @@ function cargarLocalStorage(){
     cant = localStorage.getItem("distanciaLS");
     un = localStorage.getItem("unidadesLS");
     document.getElementById("dist").value = cant + " " + un;
+}
+
+x=0;
+dx=2;
+function animarAuto(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var img = new Image();
+    img.src = "images/auto.png";
+
+    img.onload = function () {
+        ctx.drawImage(img, x, 100)
+    }
+    x+=dx;
+}
+
+function dibujarImagen(posX, posY) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var img = new Image();
+    img.src = "images/auto.png";
+
+    img.onload = function () {
+        ctx.drawImage(img, posX, posY)
+    }
+}
+
+function dibujarCirCuad() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+    var margen = 5;
+    ctx.fillStyle = "#333888";
+    ctx.fillRect(0+margen,yMax-40-margen,40, 40);
+
+    ctx.arc(xMax/2, yMax/2, 20, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "#214144"
+    ctx.fill();
+}
+
+var bandera;
+function dibujar(event){
+    var  canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function (){bandera = true};
+    canvas.onmouseup = function (){bandera = false};
+    if(bandera){
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
+    }
+}
+
+function LimpiarCanvas(){
+    var  canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
+
+function dibujarCuadriculado(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var anchoMax = canvas.width;
+    var alturaMax = canvas.height;
+    ctx.beginPath();
+    for(var i=0; i<alturaMax;){
+        ctx.moveTo(0,i);
+        ctx.lineTo(anchoMax,i);
+        ctx.strokeStyle = "#952e2e";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+    ctx.beginPath();
+    for(var i=0; i<anchoMax;){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,alturaMax);
+        ctx.strokeStyle = "#952e2e";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(0,alturaMax/2);
+    ctx.lineTo(anchoMax,alturaMax/2);
+    ctx.strokeStyle = "#312e95";
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(anchoMax/2,0);
+    ctx.lineTo(anchoMax/2,alturaMax);
+    ctx.strokeStyle = "#312e95";
+    ctx.stroke();
+    ctx.closePath();
+
 }
